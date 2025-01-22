42839951d918d00b3a91b7519f4a62fcb5de4417
Daniel <dywilby@gmail.com>
2025-01-22 11:46:12 -0800

    implement protobuf validation

--- server/src/app.py
+++ server/src/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify, send_file
 from markupsafe import escape
+import google
 
-from .proto import student_details_pb2 as StudentDetails
+from .proto import student_details_pb2 as proto
 
 # start another process p2 here to handle report card creation
 app = Flask(__name__)
@@ -23,28 +24,21 @@ def view_reports(student_id=None):
 def generate():
     """
     Endpoint to initiate the generation of a report card.
+
+    curl -X POST --data-raw $'\n\x06103348\x12\nNAme, Test(\x04' http://localhost:5000/generate
     """
-    data = parse_request(request.json)
     
-    if data is None:
-        return "Error parsing request", 400
+    student_details = proto.StudentDetails()
+
+    try:
+        data = request.get_data()
+        student_details.ParseFromString(data)
+    except google.protobuf.message.DecodeError:
+        return "Error parsing student details.\n", 400
+
+    print(student_details)
 
     # send report card data to p2
 
-    print(data)
 
-    return "Creating report card! Please check back later for results.", 200
-
-
-# Could potentially replace this with protobuf, but may be overkill
-class ReportRequest:
-    def __init__(self, name):
-        self.name = name
-
-def parse_request(json: dict[str]) -> ReportRequest | None:
-    try:
-        name = json["title"]
-
-        return ReportRequest(name)
-    except Exception:
-        return None
+    return f"Creating report card for {student_details.student_name}! Please check back later for results.\n", 200
