1ecb93e7253e0eee38c2a7215d306c4ba899f004
Daniel <dywilby@gmail.com>
2025-01-31 12:01:34 -0800

    add report card dispaly to new ui

--- ui/src/app/view/[slug]/page.tsx
+++ ui/src/app/view/[slug]/page.tsx
@@ -5,18 +5,42 @@ import { usePathname } from "next/navigation"
 
 export default function Report() {
     const pathname = usePathname()
+    const student_id = pathname.split("/").at(-1);
 
-    const [data, setData] = useState(null);
+    const [data, setData] = useState<string[][] | null>(null);
     const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
-      fetch(`http://localhost:3000/view/${pathname.split("/").at(-1)}`).then(res => res.json())
+      fetch(`http://localhost:3000/view/${student_id}`).then(res => res.json())
         .then(data => {
-          console.log(data);
           setData(data)
           setLoading(false)
         })
     }, [])
 
-    return <div className="w-1/3 mx-auto my-12"><p>{pathname}</p></div>
+    if (isLoading || data == null || data.length < 1)
+        return (<div className="w-1/3 mx-auto my-12"><h1>loading...</h1></div>);
+
+    return (
+      <div className="w-1/3 mx-auto my-12">
+        <h1 className="font-bold">{data[0][0]}</h1>
+        <h2>{student_id}</h2>
+
+        <table className="table my-8">
+          <thead>
+            <th>Date</th>
+            <th>Response</th>
+          </thead>
+          <tbody>
+            {data.map(([, report, date], index) => (
+              <tr key={index}>
+                <td>{date}</td>
+                <td>{report}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+      </div>
+    );
 }
